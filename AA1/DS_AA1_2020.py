74e19879824b44e967a5d6653b468e4b4aedeae8
Paul-Barac <63210614+Paul-Barac@users.noreply.github.com>
2020-05-12 16:18:47 +0200

    Update DS_AA1_2020.py

--- AA1/DS_AA1_2020.py
+++ AA1/DS_AA1_2020.py
@@ -54,7 +54,7 @@ jQuery(document).bind("contextmenu", function(e) {
     e.preventDefault();});
 	
 var cpt=0;
-IPython.notebook.kernel.execute("cpt = '" + 0 + "'")
+IPython.notebook.kernel.execute("cpt = '" + cpt + "'")
 
 window.addEventListener('blur', (event) => {
     cpt=cpt+1
