7a1a985fd4aa9cae992910d6d05e358fc9ea6255
Paul-Barac <63210614+Paul-Barac@users.noreply.github.com>
2020-05-22 16:20:40 +0200

    Update DS_AA1_2020.py

--- AA1/DS_AA1_2020.py
+++ AA1/DS_AA1_2020.py
@@ -1,45 +1,7 @@
-import time,IPython.core.display
+import IPython.core.display
 
-def tests(*args,num):
-    try:
-        start_test = time.time()
-        for name, value in args: globals()[name]=value
-        print("========================== début du test =========================="+'\n')
-        
-        #------------------------------------------condition(s) que l'on teste
-        #---------------------------------------------------------------------
-        if num==1:
-            pile_emp=[1,2,3]
-            empiler(pile_emp,4)
-            assert pile_emp==[1,2,3,4],"La pile retournée par la fonction empiler() n'est pas correcte"
-        
-        elif num==2:
-            pile_dep=[1,2,3]
-            depiler(pile_dep)
-            assert pile_dep==[1,2],"La pile retournée par la fonction dépiler() n'est pas correcte"
-        
-        elif num==3:
-            assert exo1(8)==[3, 6],"La pile retournée par la fonction exo1() n'est pas correcte"
-        
-        elif num==4:
-            L_exo2 = [34, 7, 52, 1, 40]
-            assert exo2(L_exo2,"croissant")==[1, 7, 34, 40, 52],"La valeur de la liste retrounée triée par ordre croissant n'est pas correcte"           
-            assert exo2(L_exo2,"décroissant")==[52, 40, 34, 7, 1],"La valeur de la liste retrounée triée par ordre décroissant n'est pas correcte"
-        #---------------------------------------------------------------------
-        #---------------------------------------------------------------------
-        stop_test = time.time()
-        tps = stop_test -start_test
-        print ("Test réussi en ",round(tps,2)," secondes",'\n')
-        print("=========================== fin du test ===========================")
-        
-    except (AssertionError, NameError) as err:
-        stop_test = time.time()
-        tps = stop_test -start_test
-        print(err,"\n")
-        print ("Test échoué en ",round(tps,2)," secondes",'\n')
-        print("=========================== fin du test ===========================")
-
-    
+print("début")
+  
 # Commandes Javascript pour bloquer les accès Ctrl+C/Ctrl+V aux étudiants + compteur de sorties
  
 jscode_cmd = """
